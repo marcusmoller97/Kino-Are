70717730d4a8566568ef14e0795dc948f6e1f2ae
marcusmoller97 <marcussmoller@icloud.com>
2025-01-19 13:58:56 +0100

    error handling

--- server.js
+++ server.js
@@ -1,50 +1,43 @@
 import express from 'express';
 import renderPage from './lib/renderPage.js';
-import { loadMovies, loadMovie } from './src/apiMovies.js';
+import {errorHandler}  from  './lib/middleware.js';
 
 const app = express();
 const PORT = 5080;
 
-app.set('view engine', 'pug');
-app.set('views', 'views');
+app
+    .set('view engine', 'pug')
+    .set('views', 'views');
 
-app.get('/', (req, res) => {
-    renderPage(res, 'home')
-});
+app
+    .get('/', (_req, res) => {
+        renderPage(res, 'home');
+    })
+    .get('/home', (_req, res) => {
+        renderPage(res, 'home');
+    })
+    .get('/movies', async (_req, res) => {
+        renderPage(res, 'movies');
+    })
+    .get('/movies/:id', async (req, res) => {
+        renderPage(res, 'movie', req.params.id);
+    });
 
-app.get('/home', (req, res) => {
-    renderPage(res, 'home');
-});
+app
+    .use('/static', express.static('./static'))
+    .use('/pictures', express.static('./pictures'))
+    .use('/content', express.static('./content'))
+    .use('/js', express.static('./js'))
 
-app.get('/movies', async (req, res) => {
-    
-    renderPage(res, 'movies')
-    /* res.render('movies') */
-});
+app.all('*', (_req, res) => res.status(404).render("404"))
 
-app.get('/movies/:id', async (req, res) => {
-    console.log(req.params.id)
-    /* const movie = await loadMovie(req.params.id) */
-    renderPage(res, 'movie', req.params.id)
-})
-
-app.use('/static', express.static('./static'));
-app.use('/pictures', express.static('./pictures'));
-app.use('/content', express.static('./content'));
-app.use('/js', express.static('./js'));
-
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
     console.log(`Server is running on: http://localhost:${PORT}/`);
 });
 
-//TODO:
-//Fix so js code and css code loads page movie after this code:
-//app.get('/movies/:id', async (req, res) => {
-//console.log(req.params.id);
-//renderPage(res, 'movie', req.params.id)
-//})
 //TODO: Filmernas intro-text ska renderas med hjälp av “markdown” för formatering Maybe done!
 //TODO: När användare besöker en filmsida som inte existerar ska en felsida visas och servern svara med korrekt HTTP-status, TODO: ett integrationstest bekräftar att detta fungerar
 //TODO: Det ska finnas ett integrationstest som verifierar att filmsidor visar rätt titel
