ff7ae5249443ba322cae2e8720cbfcf2e2b02a4c
marcusmoller97 <marcussmoller@icloud.com>
2025-01-21 17:41:51 +0100

    changed the server into function

--- server.js
+++ server.js
@@ -1,51 +1,13 @@
-import express from 'express';
-import { renderPage, renderMoviesPage, renderMoviePage } from './lib/renderPage.js';
-import { errorHandler } from './lib/middleware.js';
+import initApp from './src/app.js';
 
-const app = express();
+const app = initApp();
 const PORT = 5080;
-
-app
-    .set('view engine', 'pug')
-    .set('views', 'views');
-
-app
-    .get('/', (_req, res) => {
-        renderPage(res, 'home');
-    })
-    .get('/home', (_req, res) => {
-        renderPage(res, 'home');
-    })
-    .get('/movies', async (_req, res) => {
-        renderMoviesPage(res, 'movies');
-    })
-    .get('/movies/:id', async (req, res) => {
-        console.log(req.params.id);
-        renderMoviePage(res, 'movie', req.params.id);
-    });
-
-
-app
-    .use('/static', express.static('./static'))
-    .use('/pictures', express.static('./pictures'))
-    .use('/content', express.static('./content'))
-    .use('/js', express.static('./js'));
-
-
-app.all('*', (_req, res) => {
-    res.status(404);
-    const status = res.statusCode;
-    res.render("404", { status });
-});
-
-app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
     console.log(`Server is running on: http://localhost:${PORT}/`);
 });
 
-export { app };
 
 //TODO: När användare besöker en filmsida som inte existerar ska en felsida visas och servern svara med korrekt HTTP-status, TODO: ett integrationstest bekräftar att detta fungerar
 //TODO: Det ska finnas ett integrationstest som verifierar att filmsidor visar rätt titel
---
+++ src/app.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import { renderPage, renderMoviesPage, renderMoviePage } from '../lib/renderPage.js';
+import { errorHandler } from '../lib/middleware.js';
+
+function initApp () {
+    const app = express();
+
+    app
+        .set('view engine', 'pug')
+        .set('views', 'views');
+
+    app
+        .get('/', (_req, res) => {
+            renderPage(res, 'home');
+        })
+        .get('/home', (_req, res) => {
+            renderPage(res, 'home');
+        })
+        .get('/movies', async (_req, res) => {
+            renderMoviesPage(res, 'movies');
+        })
+        .get('/movies/:id', async (req, res) => {
+            console.log(req.params.id);
+            renderMoviePage(res, 'movie', req.params.id);
+        });
+
+
+    app
+        .use('/static', express.static('./static'))
+        .use('/pictures', express.static('./pictures'))
+        .use('/content', express.static('./content'))
+        .use('/js', express.static('./js'));
+
+
+    app.all('*', (_req, res) => {
+        res.status(404);
+        const status = res.statusCode;
+        res.render("404", { status });
+    });
+
+    app.use(errorHandler);
+
+    return app;
+}
+
+export default initApp;
