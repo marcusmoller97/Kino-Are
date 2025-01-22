89fe820675779d6faf3bddf8b85a3af38311d2c6
marcusmoller97 <marcussmoller@icloud.com>
2025-01-22 16:10:14 +0100

    removed comments

--- server.js
+++ server.js
@@ -8,8 +8,3 @@ app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
     console.log(`Server is running on: http://localhost:${PORT}/`);
 });
-
-
-//TODO: När användare besöker en filmsida som inte existerar ska en felsida visas och servern svara med korrekt HTTP-status, TODO: ett integrationstest bekräftar att detta fungerar
-//TODO: Det ska finnas ett integrationstest som verifierar att filmsidor visar rätt titel
-//TODO: 
