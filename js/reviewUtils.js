1ff1efbeef4f5d92ddcb94abb1962fd538d22c92
Christian Fryksten <christianfryksten1@gmail.com>
2025-01-29 15:36:39 +0100

    Object oriented functions for fetching review, filtering invalid reviews and date based reviews.

--- js/reviewUtils.js
+++ js/reviewUtils.js
@@ -1,33 +1,78 @@
-async function fetchRecentReviews() {
-  const API = "https://plankton-app-xhkom.ondigitalocean.app/api/reviews";
-  try {
-    const response = await fetch(API);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
+/**
+ * @author Marcus och Christian
+ */
+
+const reviewUtils = {
+  BASE_URL: "https://plankton-app-xhkom.ondigitalocean.app/api/",
+  API: [],
+  recentReviews: [],
+  /**
+   * Function to fetch
+   */
+  async fetchReviews() {
+    try {
+      const url =
+        this.BASE_URL + "reviews?populate=movie&pagination[pageSize]=1000";
+      console.log(url);
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      let reviews = await response.json();
+
+      this.API = reviews.data;
+    } catch (error) {
+      console.error(error.message);
     }
+  },
+  /**
+   *
+   */
+  fetchValidReviews() {
+    // removes movies with null value.
+    const filteredReviews = this.API.filter(
+      (item) => item.attributes.movie.data !== null
+    );
 
-    const data = await response.json();
-
+    console.log("Filtered reviews: ", filteredReviews);
+    return filteredReviews;
+  },
+  /**
+   * Function to sort on the ratings in the last 30 days.
+   */
+  fetchRecentReviews(apiCall) {
+    const storageArray = [];
     const today = new Date();
     const thirtyDaysAgo = new Date();
-    thirtyDaysAgo.setDate(today.getDate() - 365);
+    thirtyDaysAgo.setDate(today.getDate() - 30);
 
-    const recentReviews = [];
-
-    for (const review of data.data) {
+    for (const review of apiCall) {
       const reviewDate = new Date(review.attributes.createdAt);
 
       if (reviewDate >= thirtyDaysAgo) {
-        recentReviews.push(review);
+        storageArray.push(review);
       }
     }
+    this.recentReviews = storageArray;
+  },
+};
 
-    console.log(recentReviews);
+await reviewUtils.fetchReviews();
+reviewUtils.fetchRecentReviews(reviewUtils.fetchValidReviews());
+console.log("-------------------------------");
+console.log(reviewUtils.recentReviews);
+/* console.log(reviewUtils.API); */
 
-    return data;
-  } catch (error) {
-    console.error(error.message);
-  }
-}
+/* reviewUtils.fetchRecentReviews();
+console.log(reviewUtils.recentReviews); */
 
-fetchRecentReviews();
+/* reviewUtils.fetchValidReviews(); */
+
+/**
+ *  TODO:
+ * sortera fem högsta betyg
+ * sortera på film baserat på id
+ *
+ **/
