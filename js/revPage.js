e71c04ef96677f102be60361e4583c51f90bd443
kikoDevv <kiko.devv@gmail.com>
2025-01-30 19:47:14 +0100

    func to fetch rev data from api

--- js/revPage.js
+++ js/revPage.js
@@ -1,30 +1,41 @@
-createRevCard("itsmeKiko", "5", "hellow kiko");
+//---------root----------
+
+const movieId = 1;
+fetchReviews(movieId).then(reviews => createReviewCards(reviews));
+//-------func section---------
 
 
+//-------Fetch reviews for a specific movie----------
+async function fetchReviews(movieId) {
+    const response = await fetch(`https://plankton-app-xhkom.ondigitalocean.app/api/reviews?filters[movie]=${movieId}&pagination[page]=1&pagination[pageSize]=10`);
+    const data = await response.json();
+    return data.data;
+}
 
-function createRevCard( name, rating, comment) {
-    const container = document.createElement('div');
-    container.className = 'cardContainer';
+//--------Create review cards review--------
+function createReviewCards(reviews) {
+    reviews.forEach(review => {
+        const { name, rating, comment } = review.attributes;
+        createRevCard(name, rating, comment);
+    });
+}
+
+//-------func to generate review card--------
+function createRevCard(name, rating, comment) {
+    const appendTo = document.querySelector(".loadHere");
+    const container = document.createElement("div");
+    container.className = "revCard";
     container.innerHTML = `
-        <div class="imgHolder">
-            <img class="movieImg" src="pictures/1.jpeg" />
-        </div>
-        <div class="moviesRev">
-            <h1>Reviews</h1>
-            <div class="revCard">
-                <img class="revImg" src="pictures/kommenter1600.png" />
-                <div class="revInfo">
-                    <div class="name-rating">
-                        <p class="revName">${name}</p>
-                        <p class="revRating">${rating}</p>
-                    </div>
-                    <div class="revComment">
-                        <p class="comment">${comment}</p>
-                    </div>
-                </div>
+        <img class="revImg" src="pictures/kommenter1600.png" />
+        <div class="revInfo">
+            <div class="name-rating">
+                <p class="revName">${name}</p>
+                <p class="revRating">${rating}</p>
             </div>
-            <button class="backBtn">Back to home</button>
+            <div class="revComment">
+                <p class="comment">${comment}</p>
+            </div>
         </div>
     `;
-    document.body.appendChild(container);
+    appendTo.appendChild(container);
 }
