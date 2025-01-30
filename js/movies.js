3c161e869634db646e34c6fdc981a11da9de403a
kikoDevv <kiko.devv@gmail.com>
2025-01-30 21:55:20 +0100

    added paing controller with button

--- js/movies.js
+++ js/movies.js
@@ -3,6 +3,7 @@
  * @author Marcus
  */
 
+
 const movieCard = {
     movieInformationArray: [],
     /**
@@ -71,7 +72,7 @@ const movieCard = {
     /**
 
      * Function to create clickevent to open movieModal
-     * @param {*} movieArray Array to find all the information that matches the id from. 
+     * @param {*} movieArray Array to find all the information that matches the id from.
      */
     clickEventMovieModal (movieArray) {
         const section = document.querySelectorAll('article.movieWrapper');
--- js/revPage.js
+++ js/revPage.js
@@ -1,23 +1,28 @@
-//---------root----------
-
 const movieId = 1;
-fetchReviews(movieId).then(reviews => createReviewCards(reviews));
-//-------func section---------
+let currentPage = 1;
 
-
+fetchReviews(movieId, currentPage).then((reviews) => revCard(reviews));
 //-------Fetch reviews for a specific movie----------
-async function fetchReviews(movieId) {
-    const response = await fetch(`https://plankton-app-xhkom.ondigitalocean.app/api/reviews?filters[movie]=${movieId}&pagination[page]=1&pagination[pageSize]=10`);
+async function fetchReviews(movieId, page) {
+    const response = await fetch(
+        `https://plankton-app-xhkom.ondigitalocean.app/api/reviews?filters[movie]=${movieId}&pagination[page]=${page}&pagination[pageSize]=5`
+    );
     const data = await response.json();
     return data.data;
 }
 
 //--------Create review cards review--------
-function createReviewCards(reviews) {
-    reviews.forEach(review => {
+function revCard(reviews) {
+    const appendTo = document.querySelector(".loadHere");
+    appendTo.innerHTML = ''; // Clear previous reviews
+
+    reviews.forEach((review) => {
         const { name, rating, comment } = review.attributes;
         createRevCard(name, rating, comment);
     });
+
+    // Create pagination controls
+    createPaginationControls();
 }
 
 //-------func to generate review card--------
@@ -38,4 +43,32 @@ function createRevCard(name, rating, comment) {
         </div>
     `;
     appendTo.appendChild(container);
+}
+
+//-------func to create pagination controls--------
+function createPaginationControls() {
+    const previousBtn = document.querySelector(".previousBtn");
+    const forwardBtn = document.querySelector(".forwardBtn");
+
+    // Remove existing event listeners
+    const newPreviousBtn = previousBtn.cloneNode(true);
+    const newForwardBtn = forwardBtn.cloneNode(true);
+    previousBtn.parentNode.replaceChild(newPreviousBtn, previousBtn);
+    forwardBtn.parentNode.replaceChild(newForwardBtn, forwardBtn);
+
+    newPreviousBtn.addEventListener("click", () => {
+        if (currentPage > 1) {
+            currentPage--;
+            fetchReviews(movieId, currentPage).then((reviews) => revCard(reviews));
+        }
+    });
+
+    newForwardBtn.addEventListener("click", () => {
+        fetchReviews(movieId, currentPage + 1).then((reviews) => {
+            if (reviews.length > 0) {
+                currentPage++;
+                revCard(reviews);
+            }
+        });
+    });
 }
