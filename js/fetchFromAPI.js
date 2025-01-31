ee073de1fe8318228eb9ca5a1da8fe84100725f3
marcusmoller97 <marcussmoller@icloud.com>
2025-01-31 16:13:17 +0100

    added som dynamic functionallity and formateed date to print a string in day month time

--- js/fetchFromAPI.js
+++ js/fetchFromAPI.js
@@ -4,19 +4,53 @@ addEventListener('DOMContentLoaded', async () => {
 
         const id = window.location.pathname.split('/').slice(-1).pop();
 
-        const screeningContainer = document.querySelector('.screeningsTable');
-
         const response = await fetch(`/screenings/upcoming/${id}`);
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
-        const screenings = await response.json();
+        let screenings = await response.json();
 
-        console.log(screenings);
+        /* const screeningsArray = []; */
+        /* screeningsArray.push(screenings); */
+
+        /* console.log(screeningsArray); */
+        const screeningsDiv = document.querySelector('.screeningsTable');
+
+        if (screenings.length > 0) {
+            console.log("Denna är inte tom");
+
+            const screeningsH3 = document.createElement('h3');
+            const list = document.createElement('ul');
+
+            screenings.forEach((item) => {
+                // formate date to day month year time
+                const date = new Date(item.attributes.start_time);
+                const formateedDate = date.toLocaleString('sv-Se', {
+                    day: "numeric",
+                    month: "long",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    hour12: false,
+                })
+                const listItem = document.createElement('li');
+                listItem.innerHTML = formateedDate;
+                console.log(item.attributes.start_time);
+                // append listItem to list
+                list.append(listItem);
+            });
+
+            // append list to screening
+            screeningsDiv.append(list);
+
+        } else if (screenings.length === 0) {
+            const noScreenings = document.createElement('h3');
+            noScreenings.innerHTML = 'No upcoming screenings';
+
+            screeningsDiv.append(noScreenings);
+        }
 
 
-        
     } catch (error) {
         console.error(error);
     }
-}); 
+});
