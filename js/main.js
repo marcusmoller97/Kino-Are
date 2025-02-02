ef848355b5b42c96a85736382c5e392e6aa42f72
kikoDevv <kiko.devv@gmail.com>
2025-02-02 04:58:43 +0100

    Fixed the aljusted the show all movies btn to stay on center top -10

--- js/main.js
+++ js/main.js
@@ -1,20 +1,36 @@
 import { movieCard } from "./movies.js";
 import { movies } from "./getMovies.js";
+
+const topThreeContainer = document.querySelector(".topThree");
 const figureCard = document.querySelector("figure.movies");
-findTopThreeMovies();
+
+if (topThreeContainer) {
+  findTopThreeMovies();
+
+  document.querySelector(".allMoviesBtn").addEventListener("click", function () {
+    window.location.href = "movies";
+  });
+}
+
 async function findTopThreeMovies() {
-	const releasedArray = [];
-	const response = await fetch("/content/movies.json");
-	const data = await response.json();
-	const sortedMovies = data.sort((a, b) => b.rating - a.rating);
-	const top = sortedMovies.slice(0, 3);
-	generateTopThree(top);
+  const response = await fetch("/content/movies.json");
+  const data = await response.json();
+  const sortedMovies = data.sort((a, b) => b.rating - a.rating);
+  const top = sortedMovies.slice(0, 3);
+
+  if (topThreeContainer) {
+    generateTopThree(top);
+  }
 }
 
 //----function to generate top 3 movies from api----
 function generateTopThree(movies) {
-	const topThreeContainer = document.querySelector(".topThree");
-	topThreeContainer.innerHTML = `
+  if (!topThreeContainer) {
+    console.error("Container .topThree not found");
+    return;
+  }
+
+  topThreeContainer.innerHTML = `
     <div class="containerLeft">
       <img class="imgLeft" src="${movies[0].image}" alt="Image not found" />
       <div class="infoCard">
