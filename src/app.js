c09c6654194be73e7f33e8a77eb8f43bd5968d3d
kikoDevv <kiko.devv@gmail.com>
2025-02-14 13:07:47 +0100

    Imported route ratings.js to solve console error.

--- src/app.js
+++ src/app.js
@@ -1,56 +1,55 @@
-import express from 'express';
+import express from "express";
 import {
-  renderMovies,
-  renderPage,
-  renderMoviePage,
-} from '../lib/renderPage.js';
-import { errorHandler } from '../lib/middleware.js';
-import { apiRouter } from './API.js';
+	renderMovies,
+	renderPage,
+	renderMoviePage,
+} from "../lib/renderPage.js";
+import { errorHandler } from "../lib/middleware.js";
+import { apiRouter } from "./API.js";
+import { loadMovieRatings } from "./ratings.js";
 
 function initApp(API) {
-  const app = express();
+	const app = express();
 
-  app.set('view engine', 'pug').set('views', 'views');
+	app.set("view engine", "pug").set("views", "views");
 
-  app
-    .get('/', (_req, res) => {
-      renderPage(res, 'home');
-    })
-    .get('/home', (_req, res) => {
-      renderPage(res, 'home');
-    })
-    .get('/movies', async (_req, res) => {
-      /* renderMoviesPage(res, 'movies'); */ //without sending param API
-      const movies = await API;
-      renderMovies(res, 'movies', movies);
-    })
-    .get('/movies/:id', async (req, res) => {
-      renderMoviePage(res, 'movie', req.params.id);
-        })
-    /*.get('/movies/:id/rating', async (req, res) => {
-            const rating = await loadMovieRatings(req.params.id);
-            res.json({ movieId: req.params.id, rating });
-        });*/
+	app
+		.get("/", (_req, res) => {
+			renderPage(res, "home");
+		})
+		.get("/home", (_req, res) => {
+			renderPage(res, "home");
+		})
+		.get("/movies", async (_req, res) => {
+			const movies = await API;
+			renderMovies(res, "movies", movies);
+		})
+		.get("/movies/:id", async (req, res) => {
+			renderMoviePage(res, "movie", req.params.id);
+		})
+		.get("/movies/:id/rating", async (req, res) => {
+			const rating = await loadMovieRatings(req.params.id);
+			res.json({ movieId: req.params.id, rating });
+		});
 
-  app
-    .use(express.json())
-    .use(apiRouter)
-    .use('/static', express.static('./static'))
-    .use('/pictures', express.static('./pictures'))
-    .use('/content', express.static('./content'))
-    .use('/js', express.static('./js'))
-    .use("/api",apiRouter);
+	app
+		.use(express.json())
+		.use(apiRouter)
+		.use("/static", express.static("./static"))
+		.use("/pictures", express.static("./pictures"))
+		.use("/content", express.static("./content"))
+		.use("/js", express.static("./js"))
+		.use("/api", apiRouter);
 
+	app.all("*", (_req, res) => {
+		res.status(404);
+		const status = res.statusCode;
+		res.render("404", { status });
+	});
 
-  app.all('*', (_req, res) => {
-    res.status(404);
-    const status = res.statusCode;
-    res.render('404', { status });
-  });
+	app.use(errorHandler);
 
-  app.use(errorHandler);
-
-  return app;
+	return app;
 }
 
 export default initApp;
