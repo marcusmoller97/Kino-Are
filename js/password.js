ada08299781a9bab8cf6e33318af17481ac04ede
Christian Fryksten <christianfryksten1@gmail.com>
2025-03-07 15:17:34 +0100

    Require password length to be atleast 8 characters

--- js/password.js
+++ js/password.js
@@ -1,72 +1,79 @@
 const pathUrl = window.location.pathname;
 
 if (pathUrl === '/test-login') {
-    document.addEventListener('DOMContentLoaded', () => {
-        document.getElementById('loginForm').addEventListener('submit', function (event) {
-            event.preventDefault();
-            login();
-        });
-    });
+  document.addEventListener('DOMContentLoaded', () => {
+    document
+      .getElementById('loginForm')
+      .addEventListener('submit', function (event) {
+        event.preventDefault();
+        login();
+      });
+  });
 } else if (pathUrl === '/test') {
-    document.addEventListener('DOMContentLoaded', () => {
-        document.getElementById('registerForm').addEventListener('submit', function (event) {
-            event.preventDefault();
-            createAccount();
-        });
-    });
+  document.addEventListener('DOMContentLoaded', () => {
+    document
+      .getElementById('registerForm')
+      .addEventListener('submit', function (event) {
+        event.preventDefault();
+        createAccount();
+      });
+  });
 }
 
-function createAccount () {
-    const förnamn = document.getElementById('förnamn').value;
-    const efternamn = document.getElementById('efternamn').value;
-    const name = förnamn + " " + efternamn;
-    const mailadress = document.getElementById('email').value;
-    const telefon = document.getElementById('telefon').value;
-    const lösenord = document.getElementById('lösenord').value;
-    const upprepatLösenord = document.getElementById('upprepaLösenord').value;
+function createAccount() {
+  const förnamn = document.getElementById('förnamn').value;
+  const efternamn = document.getElementById('efternamn').value;
+  const name = förnamn + ' ' + efternamn;
+  const mailadress = document.getElementById('email').value;
+  const telefon = document.getElementById('telefon').value;
+  const lösenord = document.getElementById('lösenord').value;
+  const upprepatLösenord = document.getElementById('upprepaLösenord').value;
 
-    if (lösenord !== upprepatLösenord) {
+  /*   console.error('Här kommer lösenordets längd');
+  console.log(
+    '------------------------------------------------------------------'
+  ); */
 
-        // TODO: Korrekt logik för att visa felmeddelande enligt figma
-        alert('Lösenorden matchar inte');
-        return;
-    }
+  if (lösenord !== upprepatLösenord) {
+    // TODO: Korrekt logik för att visa felmeddelande enligt figma
+    alert('Lösenorden matchar inte');
+    return;
+  } else if (lösenord.length < 8) {
+    alert('lösenord måste vara minst 8 tecken!');
+    return;
+  } else if (localStorage.getItem(mailadress)) {
+    alert('Ett konto med denna mailadress finns redan!');
+    return;
+  }
 
-    if (localStorage.getItem(mailadress)) {
-        alert('Ett konto med denna mailadress finns redan!');
-        return;
-    }
+  localStorage.setItem(
+    mailadress,
+    JSON.stringify({ name, mailadress, telefon, lösenord })
+  );
 
-    Object.keys(localStorage).forEach(key => {
-        let value = localStorage.getItem(key);
-        console.log(key);
-    });
-
-    localStorage.setItem(mailadress, JSON.stringify({ name, mailadress, telefon, lösenord }));
-
-
-    console.log(förnamn, efternamn, mailadress, telefon, lösenord);
-    console.log('Nu har det submittats');
+  console.log(förnamn, efternamn, mailadress, telefon, lösenord);
+  console.log('Nu har det submittats');
 }
 
-function login () {
-    const email = document.querySelector('#username').value;
-    const password = document.querySelector('#password').value;
-    const user = JSON.parse(localStorage.getItem(email));
+function login() {
+  const email = document.querySelector('#username').value;
+  const password = document.querySelector('#password').value;
+  const user = JSON.parse(localStorage.getItem(email));
 
-    if (!localStorage.getItem(email)) {
-        alert('Fel epostadress');
-        return;
-    } else if (user.lösenord !== password) {
-        alert('Fel Lösenord');
-        return;
-    } if (localStorage.getItem(email) && user.lösenord === password) {
-        alert('Du är inloggad');
-    }
+  if (!localStorage.getItem(email)) {
+    alert('Fel epostadress');
+    return;
+  } else if (user.lösenord !== password) {
+    alert('Fel Lösenord');
+    return;
+  }
+  if (localStorage.getItem(email) && user.lösenord === password) {
+    alert('Du är inloggad');
+  }
 }
 
-function clearSession () {
-    localStorage.clear();
+function clearSession() {
+  localStorage.clear();
 }
 
 // TODO: Funktion för login med från sparad data i localStorage
